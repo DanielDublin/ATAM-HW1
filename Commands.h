cf635ecc59148e45c9c9c1dd65f2ff1d6d33fad7
pdavid-technion <103374296+pdavid-technion@users.noreply.github.com>
2024-02-04 17:34:24 +0200

    Update Commands.h

--- Commands.h
+++ Commands.h
@@ -1,27 +1,148 @@
-
 #ifndef SMASH_COMMAND_H_
 #define SMASH_COMMAND_H_
 
-#include <iostream>
 #include <vector>
+#include <iostream>
+
+#define COMMAND_ARGS_MAX_LENGTH (80)
+#define COMMAND_MAX_ARGS (20)
+
 using namespace std;
+const std::string WHITESPACE = " \n\r\t\f\v";
+const std::string COMPLEX_CHAR = "?*";
 
-//--------------------------Command------------------------------//
+
+/*---------------------CommandParser--------------------------*/
+
+
+class CommandParser
+{
+    public:
+
+        enum redirectionType
+        {
+            NONE,
+            APPEND, // >>
+            OVERRIDE, // >
+            PIPE,  // |
+            ERROR_PIPE, // |&
+            REDIRECTION_FAIL = -1
+        };
+
+        CommandParser() = delete;
+        CommandParser(std::string input);
+        ~CommandParser() = default;
+
+        std::string getRawCommanad();
+        std::string getFirstCommand();
+        std::string getSecondCommand();
+        std::string getCleanCommand();
+
+        bool getIsBackground();
+        bool getIsComplex();
+        int getArgCount();
+        int getTimeout();
+        redirectionType getRedirection();
+
+
+        std::string& operator[](int index);
+
+    private:      
+
+        const static int TIMEOUT_ARG_COUNT = 2;
+        static const int MAX_WORD_COUNT = 21;  // Command + 20 args
+
+        std::string raw_command;
+        std::string first_command;
+        std::string second_command;
+        std::string stripped_flagless_command;
+        std::string stripped_words[MAX_WORD_COUNT];
+        
+        redirectionType redirection;
+        bool is_background;
+        bool is_complex;
+        int arg_count;
+        int timeout;
+
+        static std::string cleanBackgroundCommand(std::string input);
+};
+
+
+/*---------------------Command--------------------------*/
+
+
+
 class Command {
-  //////////////////////
-  // copied from skeleton for now, to be completed by daniel
-  /////////////////////
-
-  /// TODO: Add your data members
-  public:
-  Command(string cmd_line);
+// TODO: Add your data members
+ public:
+  Command(const char* cmd_line);
   virtual ~Command();
   virtual void execute() = 0;
   //virtual void prepare();
   //virtual void cleanup();
   // TODO: Add your extra methods if needed
-
 };
+
+class BuiltInCommand : public Command {
+ public:
+  BuiltInCommand(const char* cmd_line);
+  virtual ~BuiltInCommand() {}
+};
+
+class ExternalCommand : public Command {
+ public:
+  ExternalCommand(const char* cmd_line);
+  virtual ~ExternalCommand() {}
+  void execute() override;
+};
+
+class PipeCommand : public Command {
+  // TODO: Add your data members
+ public:
+  PipeCommand(const char* cmd_line);
+  virtual ~PipeCommand() {}
+  void execute() override;
+};
+
+class RedirectionCommand : public Command {
+ // TODO: Add your data members
+ public:
+  explicit RedirectionCommand(const char* cmd_line);
+  virtual ~RedirectionCommand() {}
+  void execute() override;
+  //void prepare() override;
+  //void cleanup() override;
+};
+
+class ChangeDirCommand : public BuiltInCommand {
+// TODO: Add your data members public:
+  ChangeDirCommand(const char* cmd_line, char** plastPwd);
+  virtual ~ChangeDirCommand() {}
+  void execute() override;
+};
+
+class GetCurrDirCommand : public BuiltInCommand {
+ public:
+  GetCurrDirCommand(const char* cmd_line);
+  virtual ~GetCurrDirCommand() {}
+  void execute() override;
+};
+
+class ShowPidCommand : public BuiltInCommand {
+ public:
+  ShowPidCommand(const char* cmd_line);
+  virtual ~ShowPidCommand() {}
+  void execute() override;
+};
+
+class JobsList;
+class QuitCommand : public BuiltInCommand {
+// TODO: Add your data members public:
+  QuitCommand(const char* cmd_line, JobsList* jobs);
+  virtual ~QuitCommand() {}
+  void execute() override;
+};
+
 
 //--------------------------Job----------------------------------//
 class Job{
@@ -62,6 +183,59 @@ class JobsList{
   void addJobToList(Job* j);
   Job *getJobById(int jobId);
   void printJobsList();
+};
+
+/*
+class JobsList {
+ public:
+  class JobEntry {
+   // TODO: Add your data members
+  };
+ // TODO: Add your data members
+ public:
+  JobsList();
+  ~JobsList();
+  void addJob(Command* cmd, bool isStopped = false);
+  void printJobsList();
+  void killAllJobs();
+  void removeFinishedJobs();
+  JobEntry * getJobById(int jobId);
+  void removeJobById(int jobId);
+  JobEntry * getLastJob(int* lastJobId);
+  JobEntry *getLastStoppedJob(int *jobId);
+  // TODO: Add extra methods or modify exisitng ones as needed
+};
+*/
+
+class JobsCommand : public BuiltInCommand {
+ // TODO: Add your data members
+ public:
+  JobsCommand(const char* cmd_line, JobsList* jobs);
+  virtual ~JobsCommand() {}
+  void execute() override;
+};
+
+class KillCommand : public BuiltInCommand {
+ // TODO: Add your data members
+ public:
+  KillCommand(const char* cmd_line, JobsList* jobs);
+  virtual ~KillCommand() {}
+  void execute() override;
+};
+
+class ForegroundCommand : public BuiltInCommand {
+ // TODO: Add your data members
+ public:
+  ForegroundCommand(const char* cmd_line, JobsList* jobs);
+  virtual ~ForegroundCommand() {}
+  void execute() override;
+};
+
+class ChmodCommand : public BuiltInCommand {
+ public:
+  ChmodCommand(const char* cmd_line);
+  virtual ~ChmodCommand() {}
+  void execute() override;
 };
 
 
@@ -107,8 +281,6 @@ class SmallShell {
 };
 
 
+
+
 #endif //SMASH_COMMAND_H_
-
-
-
-
