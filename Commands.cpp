6192e0e4d671e22e59c5d42562bbcce0330be8b5
pdavid-technion <103374296+pdavid-technion@users.noreply.github.com>
2024-02-04 17:34:12 +0200

    Update Commands.cpp

--- Commands.cpp
+++ Commands.cpp
@@ -1,7 +1,264 @@
+#include <unistd.h>
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <sys/wait.h>
+#include <iomanip>
 #include "Commands.h"
 
-using namespace std;
-const std::string WHITESPACE = " \n\r\t\f\v";
+
+/*-----------------CommandParser-------------------*/
+
+
+CommandParser::CommandParser(string input) : raw_command(input), first_command(""), second_command(""), redirection(NONE), timeout(0)
+{
+    int first_whitespace_index = input.find_first_not_of(WHITESPACE);
+    int last_whitespace_index = input.find_last_not_of(WHITESPACE);
+    this->is_background = input.back() == '&';
+    this->is_complex = (input.find_first_of(COMPLEX_CHAR) != string::npos);
+
+    if (this->is_background && last_whitespace_index != string::npos)
+    {
+        input = input.substr(first_whitespace_index, last_whitespace_index - first_whitespace_index);
+    }
+
+    this->stripped_flagless_command = input;
+    
+    int start_index = 0;
+    int end_index = 0;
+    int counter = 0;
+    input.push_back(' '); //for better automation
+
+    for (int i = 0; i < input.length(); ++i)
+    {
+        if (input[i] == ' ')
+        {
+            end_index = i;
+            if (i != 0)
+            {
+                string word = input.substr(start_index, end_index - start_index);
+                this->stripped_words[counter] = word;
+                counter++;
+            }
+
+            //run over all whitespaces
+            while (input[i] == ' ')
+            {
+                i++;
+                start_index = i;
+            }
+        }
+    }
+
+    this->arg_count = counter;
+
+    // strips the timeout + dur part to act as a normal command
+    if (this->stripped_words[0] == "timeout" && counter >= 2)
+    {
+        timeout = stoi(this->stripped_words[1]);
+        arg_count -= 2;  
+        int new_index = this->stripped_flagless_command.find(this->stripped_words[2]);
+        this->stripped_flagless_command = this->stripped_flagless_command.substr(new_index);
+    }
+
+
+    // handling command based cases for stripping and inits
+    int redirection_index = input.find(">>");
+    if (redirection_index != string::npos)
+    {
+        this->first_command = cleanBackgroundCommand(input.substr(0, redirection_index));
+        this->second_command = input.substr(redirection_index + 2);
+        auto second_command_startpoint = this->second_command.find_first_not_of(WHITESPACE);
+
+        if (second_command_startpoint != string::npos)
+        {
+            this->second_command = this->second_command.substr(second_command_startpoint);
+            auto second_command_endpoint = this->second_command.find_first_of(WHITESPACE);
+            if (second_command_endpoint != string::npos)
+            {
+                this->second_command = this->second_command.substr(0, second_command_endpoint);
+            }
+            this->redirection = this->APPEND;
+            
+        }
+        else
+        {
+            this->redirection = this->REDIRECTION_FAIL;
+        }
+    }
+
+    redirection_index = input.find(">");
+    if (redirection_index != string::npos)
+    {
+        this->first_command = cleanBackgroundCommand(input.substr(0, redirection_index));
+        this->second_command = input.substr(redirection_index + 1);
+
+        auto second_command_startpoint = this->second_command.find_first_not_of(WHITESPACE);
+        if (second_command_startpoint != string::npos)
+        {
+            this->second_command = this->second_command.substr(second_command_startpoint);
+            auto second_command_endpoint = this->second_command.find_first_of(WHITESPACE);
+            if (second_command_endpoint != string::npos)
+            {
+                this->second_command = this->second_command.substr(0, second_command_endpoint);
+            }
+            this->redirection = this->OVERRIDE;
+            
+        }
+        else
+        {
+            this->redirection = this->REDIRECTION_FAIL;
+        }
+    }
+
+    redirection_index = input.find("|");
+    if (redirection_index != string::npos)
+    {
+        this->first_command = cleanBackgroundCommand(input.substr(0, redirection_index));
+        this->second_command = input.substr(redirection_index + 1);
+        this->redirection = this->PIPE;
+    }
+    
+    redirection_index = input.find("|&");
+    if (redirection_index != string::npos)
+    {
+        this->first_command = cleanBackgroundCommand(input.substr(0, redirection_index));
+        this->second_command = input.substr(redirection_index + 2);
+        this->redirection = this->ERROR_PIPE;
+    }
+    
+
+    if (this->redirection != this->NONE)
+    {
+        this->is_background = false;
+    }
+}
+
+
+string CommandParser::getRawCommanad()
+{
+    return this->raw_command;
+}
+
+
+string CommandParser::getFirstCommand()
+{
+    return this->first_command;
+}
+
+
+string CommandParser::getSecondCommand()
+{
+    return this->second_command;
+}
+
+string CommandParser::getCleanCommand()
+{
+    return this->stripped_flagless_command;
+}
+
+bool CommandParser::getIsBackground()
+{
+    return this->is_background;
+}
+
+
+bool CommandParser::getIsComplex()
+{
+    return this->is_complex;
+}
+
+int CommandParser::getArgCount()
+{
+    return this->arg_count;
+}
+
+int CommandParser::getTimeout()
+{
+    return this->timeout;
+}
+
+CommandParser::redirectionType CommandParser::getRedirection()
+{
+    return this->redirection;
+}
+
+string CommandParser::cleanBackgroundCommand(string input)
+{
+    int index = input.find_last_not_of(WHITESPACE);
+    bool is_background = (input[index] == '&');
+    int start_index = input.find_first_not_of(WHITESPACE);
+    int end_index = input.find_last_not_of(WHITESPACE);
+   
+    if (end_index != (int)std::string::npos && is_background)
+    {
+        input = input.substr(start_index, end_index - start_index);
+    }
+    return input;
+}
+
+string& CommandParser::operator[](int index)
+{
+    if (index >= 0 && index < this->arg_count)
+    {
+        if (timeout > 0)
+        {
+            index += this->TIMEOUT_ARG_COUNT;
+        }
+        return this->stripped_words[index];
+    }
+    throw std::logic_error("invalid index");
+}
+
+
+
+/*
+SmallShell::SmallShell() {
+// TODO: add your implementation
+}
+
+SmallShell::~SmallShell() {
+// TODO: add your implementation
+}
+*/
+
+
+/*
+/**
+* Creates and returns a pointer to Command class which matches the given command line (cmd_line)
+/*
+Command * SmallShell::CreateCommand(const char* cmd_line) {
+	// For example:
+/*
+  string cmd_s = _trim(string(cmd_line));
+  string firstWord = cmd_s.substr(0, cmd_s.find_first_of(" \n"));
+
+  if (firstWord.compare("pwd") == 0) {
+    return new GetCurrDirCommand(cmd_line);
+  }
+  else if (firstWord.compare("showpid") == 0) {
+    return new ShowPidCommand(cmd_line);
+  }
+  else if ...
+  .....
+  else {
+    return new ExternalCommand(cmd_line);
+  }
+  
+  return nullptr;
+}
+
+
+void SmallShell::executeCommand(const char *cmd_line) {
+  // TODO: Add your implementation here
+  // for example:
+  // Command* cmd = CreateCommand(cmd_line);
+  // cmd->execute();
+  // Please note that you must fork smash process for some commands (e.g., external commands....)
+}
+
+*/
 
 
 
