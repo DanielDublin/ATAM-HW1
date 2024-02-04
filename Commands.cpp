64b8ace5f7e688560a1792d24412cd880f244703
pdavid-technion <103374296+pdavid-technion@users.noreply.github.com>
2024-02-04 16:24:01 +0200

    Update Commands.cpp

--- Commands.cpp
+++ Commands.cpp
@@ -1,119 +1,75 @@
-#include <unistd.h>
-#include <string.h>
-#include <iostream>
-#include <vector>
-#include <sstream>
-#include <sys/wait.h>
-#include <iomanip>
 #include "Commands.h"
 
 using namespace std;
 const std::string WHITESPACE = " \n\r\t\f\v";
 
-#if 0
-#define FUNC_ENTRY()  \
-  cout << __PRETTY_FUNCTION__ << " --> " << endl;
 
-#define FUNC_EXIT()  \
-  cout << __PRETTY_FUNCTION__ << " <-- " << endl;
-#else
-#define FUNC_ENTRY()
-#define FUNC_EXIT()
-#endif
 
-string _ltrim(const std::string& s)
+//--------------------------Job----------------------------------//
+void Job::setJobID(int id) {jobID = id;}
+int Job::getJobID() {return jobID;}
+Job::status Job::getCurrentStatus() {return currentStatus;}
+void Job::setCurrentStatus(Job::status status) {currentStatus = status;}
+void Job::setCommand(Command *c) {command = c;}
+Command* Job::getCommand() {return command;}
+
+
+//--------------------------JobsList----------------------------//
+JobsList::~JobsList()
 {
-  size_t start = s.find_first_not_of(WHITESPACE);
-  return (start == std::string::npos) ? "" : s.substr(start);
+  delete &list;
 }
-
-string _rtrim(const std::string& s)
-{
-  size_t end = s.find_last_not_of(WHITESPACE);
-  return (end == std::string::npos) ? "" : s.substr(0, end + 1);
-}
-
-string _trim(const std::string& s)
-{
-  return _rtrim(_ltrim(s));
-}
-
-int _parseCommandLine(const char* cmd_line, char** args) {
-  FUNC_ENTRY()
-  int i = 0;
-  std::istringstream iss(_trim(string(cmd_line)).c_str());
-  for(std::string s; iss >> s; ) {
-    args[i] = (char*)malloc(s.length()+1);
-    memset(args[i], 0, s.length()+1);
-    strcpy(args[i], s.c_str());
-    args[++i] = NULL;
+void JobsList::deleteFinishedJobs()
+  {
+    for(unsigned int i = 0; i < list.size(); i++)
+      if (list[i]->getCurrentStatus() == Job::status::FINISHED)
+        list.erase(list.begin() + i);
+    maxJobID = 0;
+    for(unsigned int i = 0; i < list.size(); i++)
+      if (list[i]->getJobID() > maxJobID)
+        maxJobID = list[i]->getJobID();
   }
-  return i;
+void JobsList::addJobToList(Job* j)
+  {
+    JobsList::deleteFinishedJobs();
+    maxJobID++;
+    j->setJobID(maxJobID);
+    list.push_back(j);
 
-  FUNC_EXIT()
-}
-
-bool _isBackgroundComamnd(const char* cmd_line) {
-  const string str(cmd_line);
-  return str[str.find_last_not_of(WHITESPACE)] == '&';
-}
-
-void _removeBackgroundSign(char* cmd_line) {
-  const string str(cmd_line);
-  // find last character other than spaces
-  unsigned int idx = str.find_last_not_of(WHITESPACE);
-  // if all characters are spaces then return
-  if (idx == string::npos) {
-    return;
   }
-  // if the command line does not end with & then return
-  if (cmd_line[idx] != '&') {
-    return;
+Job* JobsList::getJobById(int jobId)
+  {
+    JobsList::deleteFinishedJobs();
+    Job *j = NULL;
+    for(unsigned int i = 0; i < list.size(); i++)
+      if (list[i]->getJobID() == jobId)
+        j = list[i];
+      return j;
   }
-  // replace the & (background sign) with space and then remove all tailing spaces.
-  cmd_line[idx] = ' ';
-  // truncate the command line string up to the last non-space character
-  cmd_line[str.find_last_not_of(WHITESPACE, idx) + 1] = 0;
-}
+void JobsList::printJobsList()
+  {
+    JobsList::deleteFinishedJobs();
+    for(unsigned int i = 0; i < list.size(); i++)
+      cout << "[" << i << "]   ID:" << list[i]->getJobID() << "  STATUS:" << list[i]->getCurrentStatus() << endl;
+  }
 
-// TODO: Add your implementation for classes in Commands.h 
-
-SmallShell::SmallShell() {
-// TODO: add your implementation
-}
-
+//--------------------------SmallShell------------------------//
+SmallShell::SmallShell() {}
 SmallShell::~SmallShell() {
-// TODO: add your implementation
+  delete jobs_list;
+  delete time_out_jobs_list;
 }
+int SmallShell::get_max_num_of_processes() {return MAX_NUM_OF_PROCESSES;}
+int SmallShell::get_args_max() {return ARGS_MAX;}
+int SmallShell::get_command_size_max() {return COMMAND_SIZE_MAX;}
+int SmallShell::get_process_name_max() {return PROCESS_NAME_MAX;}
 
-/**
-* Creates and returns a pointer to Command class which matches the given command line (cmd_line)
-*/
-Command * SmallShell::CreateCommand(const char* cmd_line) {
-	// For example:
-/*
-  string cmd_s = _trim(string(cmd_line));
-  string firstWord = cmd_s.substr(0, cmd_s.find_first_of(" \n"));
 
-  if (firstWord.compare("pwd") == 0) {
-    return new GetCurrDirCommand(cmd_line);
-  }
-  else if (firstWord.compare("showpid") == 0) {
-    return new ShowPidCommand(cmd_line);
-  }
-  else if ...
-  .....
-  else {
-    return new ExternalCommand(cmd_line);
-  }
-  */
-  return nullptr;
+Command* SmallShell::CreateCommand(string cmd_line)
+{
+  return NULL;
 }
-
-void SmallShell::executeCommand(const char *cmd_line) {
-  // TODO: Add your implementation here
-  // for example:
-  // Command* cmd = CreateCommand(cmd_line);
-  // cmd->execute();
-  // Please note that you must fork smash process for some commands (e.g., external commands....)
+void SmallShell::executeCommand(string cmd_line)
+{
+  CreateCommand(cmd_line)->execute();
 }
