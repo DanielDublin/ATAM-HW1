8f17eec8c19675c5a759d21132f6cb6713e51856
DanielDublin <bots4fun1@gmail.com>
2024-02-06 19:01:13 +0200

    removed unneeded text

--- Commands.cpp
+++ Commands.cpp
@@ -231,59 +231,6 @@ string& CommandParser::operator[](int index)
 }
 
 
-
-
-/*
-SmallShell::SmallShell() {
-// TODO: add your implementation
-}
-
-SmallShell::~SmallShell() {
-// TODO: add your implementation
-}
-
-
-
-
-
-Creates and returns a pointer to Command class which matches the given command line (command_line)
-
-Command * SmallShell::CreateCommand(const char* command_line) {
-	// For example:
-
-  string cmd_s = _trim(string(command_line));
-  string firstWord = cmd_s.substr(0, cmd_s.find_first_of(" \n"));
-
-  if (firstWord.compare("pwd") == 0) {
-    return new GetCurrDirCommand(command_line);
-  }
-  else if (firstWord.compare("showpid") == 0) {
-    return new ShowPidCommand(command_line);
-  }
-  else if ...
-  .....
-  else {
-    return new ExternalCommand(command_line);
-  }
-  
-  return nullptr;
-}
-
-
-void SmallShell::executeCommand(const char *command_line) {
-  // TODO: Add your implementation here
-  // for example:
-  // Command* cmd = CreateCommand(command_line);
-  // cmd->execute();
-  // Please note that you must fork smash process for some commands (e.g., external commands....)
-}
-
-
-
-
-*/
-
-
 /*----------------Built-in commands--------------------*/
 
 
@@ -393,3 +340,8 @@ void SmallShell::executeCommand(string command_line)
     }
   
 }
+
+
+
+
+
